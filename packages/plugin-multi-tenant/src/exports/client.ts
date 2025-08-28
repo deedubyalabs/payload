c2377fb4de300e8c5e6f739ea43a4a3e133a0e21
DaWaun Walls <deedubyadev@gmail.com>
2025-08-28 01:00:15 -0500

    initial

--- packages/plugin-multi-tenant/src/exports/client.ts
+++
@@ -1,3 +0,0 @@
-export { TenantField } from '../components/TenantField/index.client.js'
-export { WatchTenantCollection } from '../components/WatchTenantCollection/index.js'
-export { useTenantSelection } from '../providers/TenantSelectionProvider/index.client.js'
--- test/form-state/collections/Posts/ArrayRowLabel.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const ArrayRowLabel = () => {
-  return <p id="custom-array-row-label">This is a custom component</p>
-}
